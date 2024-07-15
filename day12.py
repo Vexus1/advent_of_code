3d7e20bf1aa1eb1d88f07aab77d53ecdba161526
Adrian Galik <adriangalik123@gmail.com>
2024-07-15 17:12:29 +0200

    day 12 chapter one

--- day12.py
+++ day12.py
@@ -1,33 +1,67 @@
-import pandas as pd
+from dataclasses import dataclass
 import os
-from collections import defaultdict
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+from icecream import ic
 
+
+@dataclass
 class HotSprings:
-    def __init__(self, trace: str):
-        self.data = self.store_data(trace)
+    data: list[str]
 
-    def store_data(self, trace: str) -> dict[str: list[int]]:
-        df = pd.read_csv(trace, delim_whitespace=True, header=None, names=["col1", "col2"])
-        records = df["col1"].tolist()
-        brokens = df["col2"].tolist()
-        brokens = [[int(i) for i in j if i != ","] for j in brokens]
-        data = {records[i]: brokens[i] for i in range(len(records))}
-        return data
+    def nondet_finite_automata(self, condition:
+                                         str, criteria: list[int]) -> int:
+        automaton_states = '.'
+        for number in criteria:
+            for _ in range(number):
+                automaton_states += '#'
+            automaton_states += '.'
+        states_dict = {0: 1}
+        new_states = {}
+        for char in condition:
+            for state in states_dict:
+                if char == '?':
+                    if state + 1 < len(automaton_states):
+                        new_states[state + 1] = new_states.get(state + 1, 0) + \
+                                                states_dict[state]
+                    if automaton_states[state] == '.':
+                        new_states[state] = new_states.get(state, 0) + \
+                                            states_dict[state]
+                elif char == '.':
+                    if (state + 1 < len(automaton_states) and 
+                        automaton_states[state + 1] == '.'):
+                        new_states[state + 1] = new_states.get(state + 1, 0) + \
+                                                states_dict[state]
+                    if automaton_states[state] == '.':
+                        new_states[state] = new_states.get(state, 0) + \
+                                            states_dict[state]
+                elif char == '#':
+                    if (state + 1 < len(automaton_states) and
+                        automaton_states[state + 1] == '#'):
+                        new_states[state + 1] = new_states.get(state + 1, 0) + \
+                                                states_dict[state]
+            states_dict = new_states
+            new_states = {}
+        result = states_dict.get(len(automaton_states) - 1, 0) + \
+                 states_dict.get(len(automaton_states) - 2, 0)
+        return result
+    
+    def sum_arrangements(self, data: list[str]) -> int:
+        sum_arr = 0
+        for record in data:
+            condition, crit = record.split(' ')
+            crit = list(map(int, crit.split(',')))
+            sum_arr += self.nondet_finite_automata(condition, crit)
+        return sum_arr
 
-    def find_arrangements(self):
-        for records, brokens in self.data.items():
-            count_springs = defaultdict(int)
-            for spring in records:
-                count_springs[spring] += 1
+    def part_one_sol(self) -> int:
+        return self.sum_arrangements(self.data)
+        
 
-            print(count_springs)
-
-
-
-
-hot_springs = HotSprings("inputs\day12_test.txt")
-print(hot_springs.find_arrangements())
-
-
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day12_test.txt'
+    PATH = 'inputs/day12.txt'
+    with open(PATH, 'r') as f:
+        data = f.read()
+    hot_springs = HotSprings(data.split('\n'))
+    ic(hot_springs.part_one_sol())
