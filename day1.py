1bbaddec4fae8e4531cfbb656891609bc6e75031
Vexus1 <adriangalik123@gmail.com>
2024-07-31 22:49:00 +0200

    rewriting day 1

--- day1.py
+++ day1.py
@@ -1,67 +1,53 @@
-import pandas as pd
 import os 
+from dataclasses import dataclass
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-data = pd.read_csv(f"inputs/day1.txt", header=None)[0].to_list()
+from icecream import ic
 
-# PART ONE
-def two_pointers(word):
-    first, last = None, None
-    i = 0
-    j = len(word)-1
-    while i <= j:
-        try:
-            first = int(word[i])
-        except:
-            i += 1
-        try:
-            last = int(word[j])
-        except:
-            j -= 1
+@dataclass
+class Trebuchet:
+    _data: str
 
-        if first is not None and last is not None:
-            return int(str(first) + str(last))
+    def __post_init__(self):
+        self.sol_one, self.sol_two = self.sum_calibration_values()
 
-print(sum([two_pointers(i) for i in data]))
+    @property
+    def text_numbers(self) -> list[str]:
+        text_list = ['one', 'two', 'three', 'four',
+                     'five', 'six', 'seven', 'eight', 'nine']
+        return text_list
 
-# PART TWO 
-str_numbers = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-               'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    def sum_calibration_values(self) -> tuple[int, int]:
+        calib_one = 0
+        calib_two = 0
+        for line in self._data:
+            num = []
+            num_text = []
+            for i, char in enumerate(line):
+                if char.isdigit():
+                    num.append(char)
+                    num_text.append(char)
+                for j, text_num in enumerate(self.text_numbers):
+                    if line[i:].startswith(text_num):
+                        num_text.append(str(j+1))
+            calib_one += int(num[0] + num[-1])
+            calib_two += int(num_text[0] + num_text[-1])
+        return calib_one, calib_two
 
-def two_pointers(word):
-    min_len_numbers = min([len(i) for i in str_numbers.keys()])
-    max_len_numbers = max([len(i) for i in str_numbers.keys()])
-    str_number_left, str_number_right = '', ''
-    first, last = None, None
-    i = 0
-    j = len(word)-1
-    while i <= j:
-        if type(first) is not int:
-            try:
-                first = int(word[i])
-            except:
-                str_number_left += word[i]
-                if len(str_number_left) > max_len_numbers:
-                    str_number_left = str_number_left[1:]
-                for k in range(0, max_len_numbers-min_len_numbers+1):
-                    if str_number_left[k:] in str_numbers.keys():
-                        first = str_numbers[str_number_left[k:]]
-                i += 1
-                
-        if type(last) is not int:
-            try:
-                last = int(word[j])
-            except:
-                str_number_right += word[j]
-                if len(str_number_right) > max_len_numbers:
-                    str_number_right = str_number_right[1:]
-                for k in range(0, max_len_numbers-min_len_numbers+1):
-                    if (str_number_right[k:])[::-1] in str_numbers.keys():
-                        last = str_numbers[(str_number_right[k:])[::-1]]
-                j -= 1
+    @property
+    def part_one_sol(self) -> int:
+        return self.sol_one
+    
+    @property
+    def part_two_sol(self) -> int:
+        return self.sol_two
 
-        if first is not None and last is not None:
-            return int(str(first) + str(last))
-        
-print(sum([two_pointers(i) for i in data]))
 
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day1_test.txt'
+    PATH = 'inputs/day1.txt'
+    with open(PATH, 'r') as f:
+        data = f.read()
+    trebuchet = Trebuchet(data.split('\n'))
+    ic(trebuchet.part_one_sol)
+    ic(trebuchet.part_two_sol)
