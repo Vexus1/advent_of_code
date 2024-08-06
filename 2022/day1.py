0150a0d3bcb2768001ecf2ccfc362102d9aaaca5
Vexus1 <adriangalik123@gmail.com>
2024-08-06 09:01:36 +0200

    day 1 part two

--- 2022/day1.py
+++ 2022/day1.py
@@ -6,26 +6,35 @@ from icecream import ic
 @dataclass
 class CalorieCounting:
     _data: str
-
-    @property
-    def find_most_calories(self) -> int:
-        most_calories = float('-inf')
+    
+    def calc_elfs_calories(self) -> list[int]:
+        calories = []
         elf_food = 0
         for line in self._data:
             if line.isdigit():
                 elf_food += int(line)
             else:
-                most_calories = max(most_calories, elf_food)
+                calories.append(elf_food)
                 elf_food = 0
-        return most_calories
+        calories.append(elf_food)
+        return calories
+    
+    def sum_top(self, top_numbers: int) -> int:
+        calories_sum = 0
+        elfs_calories = self.calc_elfs_calories()
+        for _ in range(top_numbers):
+            max_calories = max(elfs_calories)
+            calories_sum += max_calories
+            elfs_calories.remove(max_calories)
+        return calories_sum
 
     @property
     def part_one_sol(self) -> int:
-        return self.find_most_calories
+        return self.sum_top(1)
     
     @property
     def part_two_sol(self) -> int:
-        return 
+        return self.sum_top(3)
 
 
 if __name__ == '__main__':
