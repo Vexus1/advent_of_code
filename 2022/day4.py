1a1ea61f5b71f2d04200472f1f1fa72d8411118e
Vexus1 <adriangalik123@gmail.com>
2024-08-06 12:13:05 +0200

    day 4 part one

---
+++ 2022/day4.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+import os
+import re
+
+from icecream import ic
+
+@dataclass
+class CampCleanup:
+    _data: str
+
+    def create_section_set(self, section: str) -> tuple[set[int], set[int]]:
+        ranges = re.findall(r'(\d+)', section)
+        ranges = map(int, ranges)
+        min1, max1, min2, max2 = ranges
+        section_one = set(range(min1, max1 + 1))
+        section_two = set(range(min2, max2 + 1))
+        return section_one, section_two
+
+    def count_fully_contains(self) -> int:
+        counter = 0
+        for section in self._data:
+            section_one, section_two = self.create_section_set(section)
+            if (section_one.issubset(section_two) or
+                section_two.issubset(section_one)):
+                counter += 1
+        return counter
+
+    @property
+    def part_one_sol(self) -> int:
+        return self.count_fully_contains()
+    
+    @property
+    def part_two_sol(self) -> int:
+        return
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    PATH = 'inputs/day4_test.txt'
+    PATH = 'inputs/day4.txt'
+    with open(PATH, 'r') as f:
+        data = f.read()
+    camp_cleanup = CampCleanup(data.split('\n'))
+    ic(camp_cleanup.part_one_sol)
+    ic(camp_cleanup.part_two_sol)
