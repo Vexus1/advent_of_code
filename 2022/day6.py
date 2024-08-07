0914f19c6c3bfa171ac53cb320eb578d356654b7
Vexus1 <adriangalik123@gmail.com>
2024-08-07 11:14:35 +0200

    day 6 part one

---
+++ 2022/day6.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+import os
+from collections import deque
+
+from icecream import ic
+
+@dataclass
+class TuningTrouble:
+    _data: str
+
+    def find_marker_position(self, marker_len: int) -> int:
+        potential_marker = deque(self._data[:marker_len-1])
+        for i, char in enumerate(self._data[marker_len-1:]):
+            potential_marker.append(char)
+            correct = set(potential_marker)
+            if len(correct) == marker_len:
+                return i + 1 + marker_len - 1
+            potential_marker.popleft()
+
+    @property
+    def part_one_sol(self) -> int:
+        return self.find_marker_position(4)
+    
+    @property
+    def part_two_sol(self) -> int:
+        return
+    
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day6_test.txt'
+    PATH = 'inputs/day6.txt'
+    with open(PATH, 'r') as f:
+        data = f.read()
+    tuning_trouble = TuningTrouble(data)
+    ic(tuning_trouble.part_one_sol)
+    ic(tuning_trouble.part_two_sol)
