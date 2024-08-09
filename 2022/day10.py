d2d4957cb03f855b0beab519749c3fc2a7e6329b
Vexus1 <adriangalik123@gmail.com>
2024-08-09 10:18:32 +0200

    day 10 part two

--- 2022/day10.py
+++ 2022/day10.py
@@ -8,11 +8,12 @@ class CathodeRayTube:
     _data: list[str]
 
     def __post_init__(self):
-        self.cycles = [20, 60, 100, 140, 180, 220]
         self.signal_strenght = []
+        self.ctr_row = ''
+        self.cycle = 0
 
     def update_singal_strenght(self, cycle: int, X: int) -> None:
-        if cycle in self.cycles:
+        if cycle % 40 == 20:
             self.signal_strenght.append(cycle*X)
 
     def execute_program(self) -> None:
@@ -30,6 +31,35 @@ class CathodeRayTube:
                 case ['noop']:
                     cycle += 1
                     self.update_singal_strenght(cycle, X)
+
+    def update_row(self, min_pos: int, max_pos: int) -> None:
+        if self.cycle-1 in range(min_pos, max_pos):
+            self.ctr_row += '#'
+        else:
+            self.ctr_row += '.'
+        if len(self.ctr_row) == 40:
+            print(self.ctr_row)
+            self.ctr_row = ''
+            self.cycle = 0
+
+    def render_image(self) -> None:
+        X = 1
+        min_pos = 0
+        max_pos = 3
+        for line in self._data:
+            line = line.split()
+            match line:
+                case 'addx', n:
+                    self.cycle += 1
+                    self.update_row(min_pos, max_pos)
+                    self.cycle += 1
+                    self.update_row(min_pos, max_pos)
+                    min_pos += int(n)
+                    max_pos += int(n)
+                    X += int(n)
+                case ['noop']:
+                    self.cycle += 1
+                    self.update_row(min_pos, max_pos)
     
     @property
     def part_one_sol(self) -> int:
@@ -37,16 +67,16 @@ class CathodeRayTube:
         return sum(self.signal_strenght)
     
     @property
-    def part_two_sol(self) -> int:
-        return 
+    def part_two_sol(self) -> None:
+        self.render_image()
     
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    PATH = 'inputs/day10_test.txt'
+    # PATH = 'inputs/day10_test.txt'
     PATH = 'inputs/day10.txt'  
     with open(PATH, 'r') as f:
         data = f.read()
     cathode_ray_tube = CathodeRayTube(data.split('\n'))
     ic(cathode_ray_tube.part_one_sol)
-    ic(cathode_ray_tube.part_two_sol)
+    cathode_ray_tube.part_two_sol
