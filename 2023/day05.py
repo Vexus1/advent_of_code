5a2d1a685dcfb4386503a3af19881c6095282f0e
Vexus1 <adriangalik123@gmail.com>
2024-09-12 12:59:57 +0200

    day 5 part two

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,16 +1,15 @@
 from dataclasses import dataclass
+from math import e
 import os
 import re
-import math as mt
 
-from icecream import ic
+from icecream import ic  # type: ignore
 
 @dataclass
 class Fertilizer:
-    data: str
+    data: list[str]
 
     def __post_init__(self):
-        self.data = data.split('\n\n')
         self.seeds = self.seed_list()
 
     def seed_list(self) -> list[int]:
@@ -27,36 +26,60 @@ class Fertilizer:
                     break
         return seed
 
-    def min_location(self, seeds) -> list[int]:
+    def min_location(self, seeds: list[int]) -> int:
         min_loc = float('inf')
         for seed in seeds:
             min_loc = min(min_loc, self.find_location(seed))
-        return min_loc
+        return int(min_loc)
+
+    def find_intervals(self) -> list[tuple[int, int, int]]:
+        intervals = []
+        for seed in re.findall(r'(\d+) (\d+)', self.data[0]):
+            x1, dx = map(int, seed)
+            x2 = x1 + dx
+            intervals.append((x1, x2, 1))
+        return intervals
     
-    def modify_seeds_dat(self, seed) -> list[int]:
-        # new_seeds = []
+    def lowest_location_number(self) -> int:
+        intervals = self.find_intervals()
         min_loc = float('inf')
-        for i in range(len(seed)//2):
-            start = seed[i*2]
-            dt = seed[(i+1)*2-1]-1
-            stop = start + dt
-            for new_seed in range(start, stop, 100):
-                min_loc = min(min_loc, self.find_location(new_seed))
-        return min_loc
+        while intervals:
+            x1, x2, level = intervals.pop()
+            if level == 8:
+                min_loc = min(x1, min_loc)
+                continue
+            for conv in re.findall(r'(\d+) (\d+) (\d+)', self.data[level]):
+                z, y1, dy = map(int, conv)
+                y2 = y1 + dy
+                diff = z - y1
+                if x2 <= y1 or y2 <= x1:
+                    continue
+                if x1 < y1:
+                    intervals.append((x1, y1, level))
+                    x1 = y1
+                if y2 < x2:
+                    intervals.append((y2, x2, level))
+                    x2 = y2
+                intervals.append((x1 + diff, x2 + diff, level + 1))
+                break
+            else:
+                intervals.append((x1, x2, level + 1))
+        return int(min_loc)
     
+    @property
     def part_one_sol(self) -> int:
         return self.min_location(self.seeds)
 
-    def part_two_sol(self) -> int:
-        seeds = self.modify_seeds_dat(self.seeds)
-        # return self.min_location(seeds)
-        return seeds
+    @property
+    def part_two_sol(self):
+        return self.lowest_location_number()
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    # with open('inputs/day5_test.csv', 'r') as f:
-    with open('inputs/day5.csv', 'r') as f:
+    # PATH = 'inputs/day5_test.txt'
+    PATH = 'inputs/day5.txt'
+    with open(PATH, 'r') as f:
         data = f.read()
-    fertilizer = Fertilizer(data)
-    ic(fertilizer.part_one_sol())
-    # ic(fertilizer.part_two_sol())
+    fertilizer = Fertilizer(data.split('\n\n'))
+    ic(fertilizer.part_one_sol)
+    ic(fertilizer.part_two_sol)
