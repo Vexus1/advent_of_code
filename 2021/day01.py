87f1f0f7e9db4f38439ef43e3658d3196983f469
Adrian Galik <adriangalik123@gmail.com>
2024-11-28 12:08:45 +0100

    day 1 part two

--- 2021/day01.py
+++ 2021/day01.py
@@ -7,22 +7,22 @@ from icecream import ic  # type: ignore
 class SonarSweep:
     data: list[str]
 
-    def count_larger_measurements(self) -> int:
+    def count_larger_measurements(self, slice: int) -> int:
         data = list(map(int, self.data))
-        return sum(x < y for x, y in zip(data, data[1:]))
+        return sum(x < y for x, y in zip(data, data[slice:]))  
 
     @property
     def part_one_sol(self) -> int:
-        return self.count_larger_measurements()
+        return self.count_larger_measurements(1)
     
     @property
     def part_two_sol(self) -> int:
-        return 
+        return self.count_larger_measurements(3)
     
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    # PATH = 'inputs/day01_test.txt' 
+    PATH = 'inputs/day01_test.txt' 
     PATH = 'inputs/day01.txt'  
     with open(PATH, 'r') as f:
         data = f.read()
