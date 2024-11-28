b05c7ff7a74b454a50c9acd070c2f4a716b11e59
Adrian Galik <adriangalik123@gmail.com>
2024-11-28 12:03:37 +0100

    day 1 part one

---
+++ 2021/day01.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+import os
+
+from icecream import ic  # type: ignore
+
+@dataclass
+class SonarSweep:
+    data: list[str]
+
+    def count_larger_measurements(self) -> int:
+        data = list(map(int, self.data))
+        return sum(x < y for x, y in zip(data, data[1:]))
+
+    @property
+    def part_one_sol(self) -> int:
+        return self.count_larger_measurements()
+    
+    @property
+    def part_two_sol(self) -> int:
+        return 
+    
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day01_test.txt' 
+    PATH = 'inputs/day01.txt'  
+    with open(PATH, 'r') as f:
+        data = f.read()
+    sonar_sweep = SonarSweep(data.split('\n'))
+    ic(sonar_sweep.part_one_sol)
+    ic(sonar_sweep.part_two_sol)
