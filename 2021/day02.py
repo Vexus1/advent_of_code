13b821a59c7219337e90b2150c2b32bb26570ac6
Adrian Galik <adriangalik123@gmail.com>
2024-11-29 13:29:05 +0100

    day 2 part two

--- 2021/day02.py
+++ 2021/day02.py
@@ -22,19 +22,36 @@ class Dive:
                     depth -= num
         return horizontal * depth
     
+    def count_position_with_aim(self) -> int:
+        horizontal = 0
+        depth = 0 
+        aim = 0
+        for line in self.data:
+            command, num = line.split(' ')
+            num = int(num)
+            match command:
+                case 'forward':
+                    horizontal += num
+                    depth += aim * num
+                case 'down':
+                    aim += num
+                case 'up':
+                    aim -= num
+        return horizontal * depth
+    
     @property
     def part_one_sol(self) -> int:
         return self.count_position()
     
     @property
     def part_two_sol(self) -> int:
-        return 
+        return self.count_position_with_aim()
     
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    PATH = 'inputs/day02_test.txt' 
-    # PATH = 'inputs/day02.txt'  
+    # PATH = 'inputs/day02_test.txt' 
+    PATH = 'inputs/day02.txt'  
     with open(PATH, 'r') as f:
         data = f.read()
     dive = Dive(data.split('\n'))
