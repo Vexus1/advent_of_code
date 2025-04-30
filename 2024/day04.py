cef14adacd2b37629679a397510eefdfce772d57
Adrian Galik <adriangalik123@gmail.com>
2025-04-30 11:58:40 +0200

    day04 part two

--- 2024/day04.py
+++ 2024/day04.py
@@ -8,7 +8,8 @@ class CeresSearch:
     data: list[str]
 
     def __post_init__(self):
-        self.keyword = 'XMAS'
+        self.grid = self.create_grid()
+        self.result1, self.result2 = self.count_words()
 
     def create_grid(self) -> dict[complex, str]:
         grid = {}
@@ -17,27 +18,35 @@ class CeresSearch:
                 position = complex(j, i)
                 grid[position] = col
         return grid
-
+    
+    def get_word(self, pos: complex) -> str:
+        return self.grid.get(pos, '')
 
     def count_words(self) -> int:
-        grid = self.create_grid()
-        count = 0
-        for pos, _ in grid.items():
+        result1 = 0
+        result2 = 0
+        for pos, _ in self.grid.items():
+            dir: complex
             for dir in [1, 1j, 1+1j, 1-1j, -1, -1j, -1+1j, -1-1j]:
-                try:
-                    count += grid[pos] + grid[pos + dir] + \
-                    grid[pos + dir*2] + grid[pos + dir*3] == self.keyword
-                except:
-                    continue
-        return count
+                result1 += (self.get_word(pos) + \
+                            self.get_word(pos + dir) + \
+                            self.get_word(pos + dir*2) + \
+                            self.get_word(pos + dir*3) == 'XMAS')
+                if dir.imag and dir.real:
+                    result2 += ((self.get_word(pos + dir) + \
+                                 self.get_word(pos) + \
+                                 self.get_word(pos - dir) == 'MAS') and
+                                (self.get_word(pos + dir*1j) + \
+                                 self.get_word(pos - dir*1j) == 'MS'))
+        return result1, result2
 
     @property
     def part_one(self) -> int:
-        return self.count_words()
+        return self.result1
     
     @property
     def part_two(self) -> int:
-        return 
+        return self.result2
 
 
 if __name__ == '__main__':
