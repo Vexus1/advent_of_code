a901bce9b37e335b08f6c26af82bb388192023c9
vexus1 <adriangalik123@gmail.com>
2025-08-17 22:14:43 +0200

    day 7 part one 2024

---
+++ 2024/day07.py
@@ -0,0 +1,47 @@
+import os 
+from dataclasses import dataclass
+
+from icecream import ic # type: ignore
+
+@dataclass
+class BridgeRepair:
+    data: list[str]
+
+    def test_numbers(self, test: int, nums: list[int]) -> bool:
+        if nums[0] > test:
+            return False
+        elif len(nums) == 1:
+            return nums[0] == test
+        elif self.test_numbers(test, [nums[0] + nums[1]] + nums[2:]):
+            return True
+        elif self.test_numbers(test, [nums[0] * nums[1]] + nums[2:]):
+            return True
+        return False
+
+    def total_cal_result(self) -> int:
+        total = 0
+        for line in self.data:
+            test, nums = line.split(': ')
+            test = int(test)
+            nums = list(map(int, nums.split(' ')))
+            if self.test_numbers(test, nums):
+                total += test        
+        return total
+
+    @property
+    def part_one(self) -> int:
+        return self.total_cal_result()
+    
+    @property
+    def part_two(self) -> int:
+        return 
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day07_test.txt'
+    PATH = 'inputs/day07.txt'
+    with open(PATH, 'r') as f:
+        data = f.read().split('\n')
+    bridge_repair = BridgeRepair(data)
+    ic(bridge_repair.part_one)
+    # ic(bridge_repair.part_two)
