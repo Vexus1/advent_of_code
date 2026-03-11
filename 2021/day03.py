7f26951edeae29aaf3dc31602cf0870f1d41475f
vexus1 <adriangalik123@gmail.com>
2026-03-11 02:20:07 +0100

    2021 day 01 part one

---
+++ 2021/day03.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+import os
+
+from icecream import ic  # type: ignore
+
+@dataclass
+class BinaryDiagnostic:
+    data: list[str]
+
+    def transpose(self, matrix: list[str]) -> list[str]:
+        return list(map(''.join, zip(*matrix)))
+
+    def power_consumption(self) -> int:
+        transposed_report = self.transpose(self.data)
+        gamma_rate = ''
+        epsilon_rate = ''
+        for row in transposed_report:
+            ones = row.count('1')
+            zeros = len(row) - ones
+            if zeros > ones:
+                gamma_rate += '0'
+                epsilon_rate += '1'
+            else:
+                gamma_rate += '1'
+                epsilon_rate += '0'
+        power = int(gamma_rate, 2) * int(epsilon_rate, 2)
+        return power
+            
+    @property
+    def part_one_sol(self) -> int:
+        return self.power_consumption()
+    
+    @property
+    def part_two_sol(self) -> int:
+        return 
+    
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # PATH = 'inputs/day03_test.txt' 
+    PATH = 'inputs/day03.txt'  
+    with open(PATH, 'r') as f:
+        data = f.read()
+    binary_diagnostic = BinaryDiagnostic(data.split('\n'))
+    ic(binary_diagnostic.part_one_sol)
+    ic(binary_diagnostic.part_two_sol)
