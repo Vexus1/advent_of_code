8b561446a716635cd9a042b18e6f240c4ff0695f
vexus1 <adriangalik123@gmail.com>
2026-03-16 15:53:48 +0100

    2021 day04 part two

--- 2021/day04.py
+++ 2021/day04.py
@@ -6,38 +6,42 @@ from icecream import ic  # type: ignore
 @dataclass
 class Board:
     grid: list[list[int]]
+    won: bool = False
 
-    def drop(self, num: str) -> None:
+    def drop(self, num: int) -> None:
         for i, row in enumerate(self.grid):
             for j, n in enumerate(row):
                 if n == int(num):
                     self.grid[i][j] = -1
 
-    def horizontal(self) -> bool:
-        for row in self.grid:
-            if row == [-1, -1, -1, -1, -1]:
-                return True
-    
-    def vertical(self) -> bool:
-        grid = list(map(list, zip(*self.grid)))
-        for row in grid:
-            if row == [-1, -1, -1, -1, -1]:
-                return True
+    def wins(self) -> bool:
+        return (
+            any(all(value == -1 for value in row) 
+                for row in self.grid
+            )
+            or any(all(row[col] == -1 for row in self.grid) 
+                   for col in range(5)
+            )
+        )
+
+    def unmarked_sum(self) -> int:
+        return sum(value for row in self.grid 
+                   for value in row if value != -1
+        )
 
 
 @dataclass
 class GiantSquid:
     data: list[str]
 
-    def parse_data(self) -> tuple[list[str], list[Board]]:
-        draw_nums = self.data[0].split(',')
+    def parse_data(self) -> tuple[list[int], list[Board]]:
+        draw_nums = list(map(int, self.data[0].split(',')))
         board_list: list[Board] = []
-        board = []
+        board: list[list[int]] = []
         for row in self.data[2:]:
             if row == '':
                 board_list.append(Board(board))
                 board = []
-                continue
             else:
                 row = list(map(int, row.split()))
                 board.append(row)
@@ -46,30 +50,34 @@ class GiantSquid:
     
     def score(self) -> int:
         draw_nums, board_list = self.parse_data()
-        winning_board = None
-        last_num = 0
         for num in draw_nums:
             for board in board_list:
                 board.drop(num)
-                print(board)
-                if board.horizontal() or board.vertical():
-                    winning_board = board.grid
+                if board.wins():
+                    return board.unmarked_sum() * num
+    
+    def last_score(self) -> int:
+        draw_nums, board_list = self.parse_data()
+        last_board = None
+        last_num = 0
+        for num in draw_nums:
+            for board in board_list:
+                if board.won:
+                    continue
+                board.drop(num)
+                if board.wins():
+                    board.won = True
+                    last_board = board
                     last_num = num
-                    break
-            if winning_board:
-                break
-        board_sum = board_sum = sum(
-            r for row in winning_board for r in row if r != -1
-        )
-        return board_sum * int(last_num)
-            
+        return last_board.unmarked_sum() * last_num
+
     @property
     def part_one_sol(self) -> int:
         return self.score()
     
     @property
     def part_two_sol(self) -> int:
-        return 
+        return self.last_score()
     
 
 if __name__ == '__main__':
